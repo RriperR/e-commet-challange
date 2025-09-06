70b4d6cca696656a43204338f9ec913e507c88d8
RriperR <pikuci2006@gmail.com>
2025-09-06 10:02:01 +0300

    Implement async dependencies and ClickHouse optimizations

--- 3/tables.sql
+++ 3/tables.sql
@@ -2,27 +2,29 @@ CREATE TABLE test.repositories
 (
     name     String,
     owner    String,
-    stars    Int32,
-    watchers Int32,
-    forks    Int32,
+    stars    UInt32,
+    watchers UInt32,
+    forks    UInt32,
     language String,
-    updated  datetime
+    updated  DateTime
 ) ENGINE = ReplacingMergeTree(updated)
-      ORDER BY name;
+      ORDER BY (owner, name);
 
 CREATE TABLE test.repositories_authors_commits
 (
-    date        date,
+    date        Date,
     repo        String,
     author      String,
-    commits_num Int32
+    commits_num UInt32
 ) ENGINE = ReplacingMergeTree
+      PARTITION BY date
       ORDER BY (date, repo, author);
 
 CREATE TABLE test.repositories_positions
 (
-    date     date,
+    date     Date,
     repo     String,
     position UInt32
 ) ENGINE = ReplacingMergeTree
+      PARTITION BY date
       ORDER BY (date, repo);
